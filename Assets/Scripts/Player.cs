e337fac67723eff470c3df1d8eb4bdc8d1a71384
AlexZapl <alexzap0318@gmail.com>
2023-11-19 17:16:13 +0200

    Beta 0.1.0
    Commit 0.1.0

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,61 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 10.0f;
+    [SerializeField] float jumpForce = 5f;
+    bool IsGrounded;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] Animator anim;
+    Vector3 direction;
 
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        print(IsGrounded);
+        float MoveHor = Input.GetAxis("Horizontal");
+        float MoveVer = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        direction = new Vector3(MoveHor, 0, MoveVer);
+        direction = transform.TransformDirection(direction);
+        if (MoveHor != 0 && MoveVer != 0)
+        {
+            anim.SetBool("Walk", true);
+        }
+        else;
+        {
+            anim.SetBool("Walk", false);
+        }
+        if (IsGrounded = true)
+        {
+            anim.SetBool("Jump", false);
+        }
+        else;
+        {
+            anim.SetBool("Jump", true);
+        }
 
-        transform.position = transform.position + movement * speed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded == true)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+            IsGrounded = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + direction * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        IsGrounded = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Fluid/lava":
+                    //nothing here
+                    break;
+        }
     }
 }
